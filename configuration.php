9fb2f6ff332bebb5ba14edaed3747d24ae9599a6
Tanish Garg <83292277+TheLostPrime12@users.noreply.github.com>
2021-09-21 15:01:50 +0530

    initail init

---
+++ configuration.php
@@ -0,0 +1,12 @@
+<?php
+
+$keyId = 'rzp_test_nJUO7xJTnJ1aSi';
+$keySecret = '3kXciZ4WPSvLAxl5rJnQ32wp';
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to configuration.php to report any errors
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
